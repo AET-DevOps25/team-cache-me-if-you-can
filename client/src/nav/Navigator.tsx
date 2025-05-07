67ab4a562dd15c2fe33419d903cdfee99985af47
xyz <xiyuezheng@outlook.com>
2025-05-07 09:35:41 +0200

    merged with general/setting-up branch to make sure the strucure clean and concurrent.

---
+++ client/src/nav/Navigator.tsx
@@ -0,0 +1,40 @@
+import './style_nav.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+export default function Navigator() {
+  const [username, setUsername] = useState(null);
+  const navigate = useNavigate();
+
+  function getUsername() {
+    //TODO: get authentication info from api
+  }
+
+  function logout() {
+    navigate('/');
+    setUsername(null);
+  }
+
+  return (
+    <div className="Nav">
+      <div className="topLeft">
+        <h1 className="logo" onClick={() => navigate('/')}>StudySync</h1>
+      </div>
+      <div className="topRight">
+        {username ? (
+          <>
+            <Link to="/profile" className="nav-link">{username}</Link>
+            <span className="link-separator"> | </span>
+            <a onClick={logout} className="nav-link logOut">Log out</a>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="nav-link">Login</Link>
+            <span className="link-separator"> | </span>
+            <Link to="/register" className="nav-link">Register</Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
