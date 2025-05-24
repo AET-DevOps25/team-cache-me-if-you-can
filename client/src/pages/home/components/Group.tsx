e26f5420bd34bfa667185d06e755cf3895459ea6
root <root@xyzh>
2025-05-24 11:23:22 +0200

    replace group tile with a fall back image for the future work using user uploaded images.

--- client/src/pages/home/components/Group.tsx
+++ client/src/pages/home/components/Group.tsx
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./group.css";
+import defaultImg from "../../../local_img/default.jpg";
 
 export default function Group() {
   const [groups, setGroups] = useState<Array<{
     id: number;
     name: string;
+    imageUrl: string;
   }> | null>(null);
   const [userName, setUserName] = useState<string | null>(null);
   const navigate = useNavigate();
 
   async function getAllGroups() {
     setGroups([
-      { id: 1, name: "Biology 101" },
-      { id: 2, name: "Computer Science" },
-      { id: 3, name: "Psychology" },
+      { id: 1, name: "Biology 101", imageUrl: defaultImg },
+      { id: 2, name: "Computer Science", imageUrl: defaultImg },
+      { id: 3, name: "Psychology", imageUrl: defaultImg },
     ]);
     //TODO: get all groups in no authentication info
   }
 
   async function getMyGroups() {
-    setGroups([{ id: 1, name: "Biology 101" }]);
+    setGroups([{ id: 1, name: "Biology 101", imageUrl: defaultImg }]);
     //TODO: get groups if user logged in
   }
 
@@ -61,15 +63,17 @@ export default function Group() {
         <button className="groups-button">find</button>
       </div>
 
-      {/* Group Tiles Container */}
-      <div className="groups-tiles-container">
+      {/* Groups Container */}
+      <div className="groups-img-container">
         {groups ? (
           groups.map((group) => (
             <div key={group.id} className="group-item">
               <div
-                className="group-tile"
+                className="group-image"
                 onClick={() => onGroupSelect(group.name)}
-              />
+              >
+                <img src={group.imageUrl} alt="group image"></img>
+              </div>
               <h3>{group.name}</h3>
             </div>
           ))
