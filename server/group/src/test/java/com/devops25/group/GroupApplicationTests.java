3ed76415aa0bcfef78587ee17240f872bb2da189
Jasmina Vulovic <vulovicjasmina@yahoo.com>
2025-06-26 14:23:06 +0200

    This commit just initialized the group microservice, it HASNT BEEN ADDED to the docker compose etc. For now it just exists. The conternization and rest will be added in another pr

---
+++ server/group/src/test/java/com/devops25/group/GroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops25.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
