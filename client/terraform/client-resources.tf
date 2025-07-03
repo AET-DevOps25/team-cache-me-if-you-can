4c444d54563184fae5bc77874cc1929442a9678e
xzyh <root@xyzh>
2025-07-03 10:48:30 +0200

    test deploy cluster using tf.

---
+++ client/terraform/client-resources.tf
@@ -0,0 +1,117 @@
+# Deployment
+resource "kubernetes_deployment" "client" {
+  metadata {
+    name      = "client"
+    namespace = "dev"
+    labels = {
+      app = "client"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "client"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "client"
+        }
+      }
+
+      spec {
+        container {
+          name  = "client"
+          image = "ghcr.io/aet-devops25/team-cache-me-if-you-can/client:amd64-latest"
+          image_pull_policy = "Always"
+
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service
+resource "kubernetes_service" "client_service" {
+  metadata {
+    name      = "client-service"
+    namespace = "dev"
+  }
+
+  spec {
+    selector = {
+      app = "client"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 3000
+      target_port = 3000
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+# Ingress
+resource "kubernetes_ingress_v1" "client_ingress" {
+  metadata {
+    name      = "client-ingress"
+    namespace = "dev"
+    annotations = {
+      "cert-manager.io/cluster-issuer"                 = "letsencrypt-prod"
+      "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+
+    tls {
+      hosts       = ["dev-client.student.k8s.aet.cit.tum.de"]
+      secret_name = "cache-me-if-you-can-client-tls"
+    }
+
+    rule {
+      host = "dev-client.student.k8s.aet.cit.tum.de"
+
+      http {
+        path {
+          path      = "/api/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = "gateway-service"
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = kubernetes_service.client_service.metadata[0].name
+              port {
+                number = 3000
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
