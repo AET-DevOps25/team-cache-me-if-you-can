d20b5e7f1146e09af80d4b706360bc1b4ba2779c
Jasmina Vulovic <vulovicjasmina@yahoo.com>
2025-07-08 12:16:57 +0200

    Publish server images git actions - currently works only by pushing to this branch(testing)

--- server/group/src/main/java/com/devops25/group/GroupService.java
+++ server/group/src/main/java/com/devops25/group/GroupService.java
@@ -6,7 +6,7 @@ import com.devops25.group.dto.UpdateGroupRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional; // Import Transactional
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
