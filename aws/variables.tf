eb21cf96679224ba3c018f003840163cb4a25724
xiyue <xiyue.zheng@tum.de>
2025-07-17 15:12:55 +0200

    delete tf for aws

--- aws/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "client_image" {
-  description = "GHCR Docker image"
-  default     = "ghcr.io/aet-devops25/team-cache-me-if-you-can/client"
-}
